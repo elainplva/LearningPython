b52bda9e24951bc3bd7b2241a8c778583f3855ed
elainplva <s00250500@atu.ie>
2025-10-08 11:11:29 +0100

    learning to make keyboard shortcuts and using linting formating and autopep8

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 a = 10
 print(a)
 
+X = 1
+# making keyboard shortcuts open command palette
+# Ctrl + Shift + P
+# type open keyboard shortcuts
+# type run python file in the search 
+# double click and make a shortcut
+# Ctrl + R
