571cd98afba80cf16121ceea8f82cd6a5696a45d
elainplva <s00250500@atu.ie>
2025-10-08 12:41:31 +0100

    generating a .pylintrc file and editing controls for pylint

---
+++ variables.py
@@ -0,0 +1,20 @@
+# using triple quotes for multi line strings
+
+message = '''
+Hi John,
+Here is our first email to you.
+Thank you,  
+The support team
+'''
+#len() # function to get length of string
+
+print(len(message))
+
+students_count = 1000
+rating = 4.99
+is_published = True # bullon values
+course = "Python Programming"
+print(course[0]) # first character
+print(course[-1]) # last character
+print(course[0:3]) # slicing
+
