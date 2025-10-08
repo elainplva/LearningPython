9d1048a453af14c1b4391a205c970790783b3edb
elainplva <s00250500@atu.ie>
2025-10-08 10:57:47 +0100

    installing python extension

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,12 @@
-print("hello, world!")
-print("*" * 10)
 
+print("*" * 10)
+print("Hello World")
+
+
+#Linting highlights semantic and stylistic problems in your
+# Python source code, which often helps you identify and correct 
+# subtle programming errors or coding practices that can lead to errors. 
+# For example, linting can detect the use of an undefined variable, calls 
+# to undefined functions, missing parentheses, and even more subtle issues
+#  such as attempting to redefine built-in types or functions.
 
